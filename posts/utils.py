c6afec4a55c7205f2d04688ccc7d9ad346780edc
Your Name <your_email@whatever.com>
2022-06-07 16:52:27 +0300

    fix city API

--- posts/utils.py
+++ posts/utils.py
@@ -4,7 +4,7 @@ from django_formica.settings import API_KEY
 
 
 def get_temp():
-    url = 'https://api.weather.yandex.ru/v2/informers?lat=5&lon=10'
+    url = 'https://api.weather.yandex.ru/v2/informers?lat=55.7887&lon=49.1221'
     headers = {'X-Yandex-API-Key': API_KEY}
     res = requests.get(url, headers=headers)
     try:
